064fe50767e0ca8e1814fd521d3704d71bbd4215
Entropia-dev <54455913+Entropia-dev@users.noreply.github.com>
2020-04-30 16:33:55 -0300

    1ercomit

--- TP2PROG3G1/AgregarSucursal.aspx.designer.cs
+++ TP2PROG3G1/AgregarSucursal.aspx.designer.cs
@@ -42,13 +42,13 @@ namespace TP2PROG3G1
         protected global::System.Web.UI.WebControls.TextBox txtDesc;
 
         /// <summary>
-        /// Control DropDownList1.
+        /// Control ddlpcias.
         /// </summary>
         /// <remarks>
         /// Campo generado automáticamente.
         /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
         /// </remarks>
-        protected global::System.Web.UI.WebControls.DropDownList DropDownList1;
+        protected global::System.Web.UI.WebControls.DropDownList ddlpcias;
 
         /// <summary>
         /// Control TextBox1.
---
+++ TP2PROG3G1/Conecciones.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TP2PROG3G1
+{
+    public class Conecciones
+    {
+    }
+}
